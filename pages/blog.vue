4064405eca208902d8eba37aa62524d7cc8b3548
ryoichi <ryoichi_hello@outlook.com>
2023-12-28 08:28:21 +0900

    blog.vue

---
+++ pages/blog.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h2>My Blog</h2>
+      <ul>
+        <li>
+          <h3>Blog Post 1</h3>
+          <p>This is the content of my first blog post.</p>
+        </li>
+        <li>
+          <h3>Blog Post 2</h3>
+          <p>This is the content of my second blog post.</p>
+        </li>
+        <!-- Add more blog posts as needed -->
+      </ul>
+      <nuxt-link to="/">Back to Home</nuxt-link>
+      <nuxt-link to="/projects">View Projects</nuxt-link>
+      <nuxt-link to="/contact">Contact Me</nuxt-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Blog',
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
