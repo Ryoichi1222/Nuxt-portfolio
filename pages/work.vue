61e5b4de11ef9313efd5267b8f2a9ae8632bb90d
ryoichi <ryoichi_hello@outlook.com>
2023-12-27 21:58:30 +0900

    work.vue

---
+++ pages/work.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h2>My Projects</h2>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <!-- Add more projects as needed -->
+      </ul>
+      <nuxt-link to="/">Back to Home</nuxt-link>
+      <nuxt-link to="/contact">Contact Me</nuxt-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Projects',
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
