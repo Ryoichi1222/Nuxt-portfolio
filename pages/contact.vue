833013c452976c772ff911f26ea4b6e550911d66
ryoichi <ryoichi_hello@outlook.com>
2023-12-27 21:58:05 +0900

    contact.vue

---
+++ pages/contact.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h2>Contact Me</h2>
+      <p>Feel free to reach out to me through the following channels:</p>
+      <ul>
+        <li>Email: your.email@example.com</li>
+        <li>Twitter: @yourtwitterhandle</li>
+        <!-- Add more contact information as needed -->
+      </ul>
+      <nuxt-link to="/">Back to Home</nuxt-link>
+      <nuxt-link to="/projects">View Projects</nuxt-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Contact',
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
