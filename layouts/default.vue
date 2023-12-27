e3c5cfac5a6020801998bf07fc81966981f797d7
ryoichi <ryoichi_hello@outlook.com>
2023-12-28 08:28:02 +0900

    default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,12 @@
     <header>
       <h1>My Portfolio</h1>
       <nav>
-        <nuxt-link to="/">Home</nuxt-link>
-        <nuxt-link to="/projects">Projects</nuxt-link>
-        <nuxt-link to="/contact">Contact</nuxt-link>
+        <div class="nav-links">
+          <nuxt-link to="/">Home</nuxt-link>
+          <nuxt-link to="/projects">Projects</nuxt-link>
+          <nuxt-link to="/blog">Blog</nuxt-link>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+        </div>
       </nav>
     </header>
 
@@ -31,15 +34,22 @@ header {
   background-color: #333;
   color: #fff;
   padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 nav {
   display: flex;
 }
 
+.nav-links {
+  display: flex;
+}
+
 nav nuxt-link {
-  margin-right: 1rem;
-  color: #fff;
+  margin-right: 10rem;
+  color: rgb(240, 237, 237);
   text-decoration: none;
 }
 
