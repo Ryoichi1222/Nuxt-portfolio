1247f2bea4a774c38bd67d834891589ed097904b
ryoichi <ryoichi_hello@outlook.com>
2023-12-28 21:31:29 +0900

    nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - ryo-nuxt-site',
