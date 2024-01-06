7d651b2f663f477419150472d7f65dd80cb28f5c
ryoichi <ryoichi_hello@outlook.com>
2024-01-06 21:53:55 +0900

    contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,26 +1,32 @@
 <template>
-    <div>
-      <h2>Contact Me</h2>
-      <p>Feel free to reach out to me through the following channels:</p>
-      <ul>
-        <li>Email: your.email@example.com</li>
-        <li>Twitter: @yourtwitterhandle</li>
-        <!-- Add more contact information as needed -->
-      </ul>
-      <nuxt-link to="/">Back to Home</nuxt-link>
-      <nuxt-link to="/projects">View Projects</nuxt-link>
-    </div>
-  </template>
+  <div>
+    <h1>Contact us</h1>
+    <form action="https://ryoichi.form.newt.so/v1/7DDu99Gwd" method="post">
+      <label for="name">Name</label>
+      <input id="name" name="name">
+      <label for="email">Email</label>
+      <input id="email" name="email" type="email">
+      <label for="message">Message</label>
+      <textarea id="message" name="message" />
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
     
   
   <script>
   export default {
     name: 'Contact',
+    meta: [{ name: 'description', content: 'NewtとNuxtを利用した問い合わせフォームです' }]
   }
   </script>
   
   <style scoped>
-  
+  div {
+    margin-bottom: 500px;
+  }  
 
   </style>
   
