9a388bf11a798e55157c50a5286264bf62ee9d30
ryoichi <ryoichi_hello@outlook.com>
2024-02-04 16:57:13 +0900

    project.vue

--- pages/project.vue
+++ pages/project.vue
@@ -20,6 +20,24 @@
                 <p>作品3の説明文や特徴などをここに記述します。</p>
              <a href="#">詳細を見る</a>
             </div>
+            <div class="project">
+                <img src="https://via.placeholder.com/300x200">
+                <h2>作品の名前</h2>
+                <p>作品4の説明文や特徴などをここに記述します。</p>
+             <a href="#">詳細を見る</a>
+            </div>
+            <div class="project">
+                <img src="https://via.placeholder.com/300x200">
+                <h2>作品の名前</h2>
+                <p>作品5の説明文や特徴などをここに記述します。</p>
+             <a href="#">詳細を見る</a>
+            </div>
+            <div class="project">
+                <img src="https://via.placeholder.com/300x200">
+                <h2>作品の名前</h2>
+                <p>作品6の説明文や特徴などをここに記述します。</p>
+             <a href="#">詳細を見る</a>
+            </div>
         </div>
     </div>
 </template>
